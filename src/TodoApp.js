e8d4e680594c7714cb8e09cf99ab672c6128a102
Elnaz Nasiri <Elnaz.Nasiri@MahanAir.Aero>
2020-06-01 20:37:33 +0430

    'todoappsimepleforreact'

---
+++ src/TodoApp.js
@@ -0,0 +1,50 @@
+import React from "react";
+import TodoList from "./TodoList";
+import "./Main.css";
+
+class TodoApp extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { items: [], text: "" };
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>TODO</h3>
+        <form onSubmit={this.handleSubmit}>
+          <label htmlFor='new-todo'>What needs to be done?</label>
+          <input
+            id='new-todo'
+            onChange={this.handleChange}
+            value={this.state.text}
+          />
+          <button>Add</button>
+          <TodoList items={this.state.items} />
+        </form>
+      </div>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({ text: e.target.value });
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    if (this.state.text.length === 0) {
+      return;
+    }
+    const newItem = {
+      text: this.state.text,
+      id: Date.now(),
+    };
+    this.setState((state) => ({
+      items: state.items.concat(newItem),
+      text: "",
+    }));
+  }
+}
+export default TodoApp;
